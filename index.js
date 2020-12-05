6257956386a6d8a1f1528fcf34f228a043f9a2b6
unknown <imoqbel@kiitos-tech.com>
2020-12-05 11:58:29 +0200

    format code

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ var deathsCtx = document.getElementById('deathsChart').getContext('2d');
 var chart = new Chart(casesCtx, {
     type: 'line',
     data: {
-        labels: data.map(e=> `${e.Date.split('/')[1]}/${e.Date.split('/')[0]}`),
+        labels: data.map(e => `${e.Date.split('/')[1]}/${e.Date.split('/')[0]}`),
         datasets: [{
             label: 'Cases',
             backgroundColor: 'rgb(3,144,249)',
             borderColor: 'rgb(3,144,249)',
-            data: data.map(e=> e.Cases)
+            data: data.map(e => e.Cases)
         }]
     },
     options: {}
@@ -18,12 +18,12 @@ var chart = new Chart(casesCtx, {
 var chart = new Chart(deathsCtx, {
     type: 'line',
     data: {
-        labels: data.map(e=> `${e.Date.split('/')[1]}/${e.Date.split('/')[0]}`),
+        labels: data.map(e => `${e.Date.split('/')[1]}/${e.Date.split('/')[0]}`),
         datasets: [{
             label: 'Daily Deaths',
             backgroundColor: 'rgb(179,95,82)',
             borderColor: 'rgb(179,95,82)',
-            data: data.map(e=> e["Daily deaths"])
+            data: data.map(e => e["Daily deaths"])
         }]
     },
     options: {}
